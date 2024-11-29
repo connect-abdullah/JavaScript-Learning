0158650db5e3e95d45ad9ea3b8abd8814fbb9939
M Abdullah <150430204+connect-abdullah@users.noreply.github.com>
2024-11-29 23:23:08 +0500

    Update script.js

--- Sets & Map/script.js
+++ Sets & Map/script.js
@@ -55,3 +55,60 @@ countryMap.set(country, country.length); // Key: country, Value: number of chara
 
 console.log(countryMap);
 */
+
+
+
+
+
+
+// Level 3
+
+// Use the countries data to find the 10 most spoken languages:
+
+/* 
+  const countries = [
+    { name: 'United States', languages: ['English'] },
+    { name: 'Canada', languages: ['English', 'French'] },
+    { name: 'France', languages: ['French'] },
+    { name: 'Russia', languages: ['Russian'] },
+    { name: 'Spain', languages: ['Spanish'] },
+    { name: 'Portugal', languages: ['Portuguese'] },
+    { name: 'Germany', languages: ['German'] },
+    { name: 'Netherlands', languages: ['Dutch'] },
+    { name: 'China', languages: ['Chinese'] },
+    { name: 'Kenya', languages: ['Swahili', 'English'] },
+    { name: 'Tanzania', languages: ['Swahili', 'English'] },
+  ];
+  
+  // Step 1: Count the languages
+  const languageCounts = {};
+  countries.forEach(country => {
+    country.languages.forEach(language => {
+      if (languageCounts[language]) {
+        languageCounts[language]++;
+      } else {
+        languageCounts[language] = 1;
+      }
+    });
+  });
+  
+  // Step 2: Convert the counts to an array
+  const languageArray = Object.entries(languageCounts).map(([language, count]) => {
+    return { [language]: count };
+  });
+  
+  // Step 3: Sort the array by count
+  const sortedLanguages = languageArray.sort((a, b) => {
+    const countA = Object.values(a)[0];
+    const countB = Object.values(b)[0];
+    return countB - countA;
+  });
+  
+  // Step 4: Get the top N languages
+  function mostSpokenLanguages(countries, topN) {
+    return sortedLanguages.slice(0, topN);
+  }
+  
+  console.log(mostSpokenLanguages(countries, 10));
+  console.log(mostSpokenLanguages(countries, 3));
+   */
